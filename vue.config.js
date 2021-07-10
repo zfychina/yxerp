edead13679bfe3c1be2c80178d524eb7a3b4f023
zfychina <chenlang1983@163.com>
2021-07-10 18:52:29 +0800

    '设置目录别名'

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'components': '@/components',
+        'store': '@/store',
+        'views': '@/views',
+        'network': '@/network',
+        'utils': '@/utils',
+      }
+    }
+
+  },
+  publicPath: './'
+}
