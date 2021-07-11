a15f47a60d26aaf9f61c5fc23a0fb44cfebd887a
zfychina <chenlang1983@163.com>
2021-07-11 18:52:26 +0800

    '首页列表'

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant';
-import 'vant/lib/index.css';
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
 
 createApp(App)
   .use(store)
   .use(router)
   .use(Vant)
+  .use(ElementPlus)
 .mount('#app')
--- src/views/shortmold/Shortmold.vue
+++ src/views/shortmold/Shortmold.vue
@@ -4,11 +4,223 @@
 <!--      <van-icon name="search" size="18" />-->
 <!--    </template>-->
 <!--  </van-nav-bar>-->
-
+  <van-sticky>
   <van-dropdown-menu active-color="#1989fa">
     <van-dropdown-item v-model="state.value1" :options="option1" />
-    <van-dropdown-item v-model="state.value2" :options="option2" />
   </van-dropdown-menu>
+<!--表头-->
+    <table style="width: 100%">
+      <thead>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%" @click="sorttable">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      </thead>
+    </table>
+
+  </van-sticky>
+
+  <van-pull-refresh v-model="state.refreshing" @refresh="onRefresh">
+    <van-list
+        v-model:loading="state.loading"
+        :finished="state.finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+    >
+<!--      <van-cell v-for="item in state.list" :key="item" :title="item">-->
+    <table style="width: 100%">
+      <tbody>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      <tr>
+        <th style="width: 20%">物料编号</th>
+        <th style="width: 30%">物料名称</th>
+        <th style="width: 15%">需求量</th>
+        <th style="width: 15%">缺料数</th>
+        <th style="width: 20%">供应商</th>
+      </tr>
+      </tbody>
+    </table>
+
+    </van-list>
+  </van-pull-refresh>
+
 </template>
 
 <script>
@@ -18,31 +230,108 @@ export default {
 
   name: "Shortmold",
   setup() {
+
     const state = reactive({
+      // 导航栏
       value1: 0,
-      value2: 'a',
+      // 列表
+      list: [],
+      loading: false,
+      finished: false,
+      refreshing: false,
+
+      month: {page:0, list: []},
+      amig: {page:0, list: []},
+      apecs: {page:0, list: []},
+
     });
     const option1 = [
-      { text: '7月份缺料', value: 0 },
+      { text: '本月缺料', value: 0 },
       { text: 'AMIG缺料', value: 1 },
       { text: 'APECS缺料', value: 2 },
     ];
-    const option2 = [
-      { text: '默认排序', value: 'a' },
-      { text: '物料编号排序', value: 'b' },
-      { text: '缺料数排序', value: 'c' },
-    ];
+
+    // 列表
+    const onLoad = () => {
+      setTimeout(() => {
+        if (state.refreshing) {
+          state.list = [];
+          state.refreshing = false;
+        }
+
+        for (let i = 0; i < 10; i++) {
+          state.list.push(state.list.length + 1);
+        }
+        state.loading = false;
+
+        if (state.list.length >= 40) {
+          state.finished = true;
+        }
+      }, 1000);
+    };
+
+    const onRefresh = () => {
+      // 清空列表数据
+      state.finished = false;
+
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      state.loading = true;
+      onLoad();
+    };
+    // 排序
+    const sorttable = () => {
+      console.log("llsjldfjlas")
+    }
+
 
     return {
+      // 导航栏
       state,
       option1,
-      option2,
+
+      // 列表
+      onLoad,
+      onRefresh,
+      sorttable,
+
     };
 
   },
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+table {
+  border-collapse: collapse;
+  thead {
+    background-color: #ffffff;
+    tr {
+      height: 40px;
+      th{
+        text-align: left;
+        padding: 10px ;
+        font-size: 12PX;
+        color: var(--color-high-text);
+        font-weight: 800;
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      th {
+        text-align: left;
+        padding: 10px ;
+        font-size: 10PX;
+        color: var(--color-text);
+        font-weight: 400;
+
+        border-bottom: 1pt solid #efecec;
+      }
+    }
+}
+}
+
 
 </style>
